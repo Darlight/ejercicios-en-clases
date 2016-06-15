77d91c75b2a40d0d03479b3048b1c74ee8783c89
Darlight <andres.perdomo09@gmail.com>
2016-06-15 08:28:48 -0600

    Recursion #1

--- Maximo Numero/maximo.py
+++ Maximo Numero/maximo.py
@@ -1,7 +1,14 @@
 def maximo_numero(lista):
-	if lista[0] < lista[1]:
-		return 
-	return lista[: :+1]*maximo_numero(numeros)
+	#Caso Base
+	if len(lista) == 1:
+		return lista[0]
+	#Procedimiento de menor a mayor
+	sublista = (lista[1:])
+	submaximo = maximo_numero(sublista)
+	if lista[0] > submaximo:
+		return lista[0]
+	return submaximo
 
-numeros = [40,30,400,600,100]
+
+numeros = [12,344,54325,545,23,677,1534243]	
 print(maximo_numero(numeros))
