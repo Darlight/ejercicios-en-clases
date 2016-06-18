c8ca2eabbc596936e9e8ba57ae6c51c6caf92e24
Darlight <andres.perdomo09@gmail.com>
2016-06-17 21:48:31 -0500

    Terminando palindromos

--- Pilas y Colas/palindromos.py
+++ Pilas y Colas/palindromos.py
@@ -2,10 +2,24 @@ from collections import deque
 def es_palindromo(palabra):
 	pila = []
 	cola = deque([])
+	palindromo = (" ")
+	verdadero = True
 	for letra in palabra:
 		pila.append(letra) # Pop
 		cola.append(letra) # popleft
-		if pila.pop() == cola.popleft():
-			return True
+	maximo = len(pila)
+	while verdadero:
+		if maximo >= 0:
+			palindromo = palindromo + pila[maximo]
+			maximo = maximo - 1
 		else:
-			return False
+			verdadero = False
+	if palindromo == palabra:
+		return True
+	else:
+		return False
+
+
+
+nombre = ("ana")
+print (es_palindromo(palabra))
