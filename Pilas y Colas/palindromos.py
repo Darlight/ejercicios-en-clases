aec50ab2029ca912b34259f3980eb84b3e357165
Darlight <andres.perdomo09@gmail.com>
2016-07-04 08:35:07 -0600

    Palindromos ejercicio completo
    Gracias Gabriel por tu ayuda

--- Pilas y Colas/palindromos.py
+++ Pilas y Colas/palindromos.py
@@ -3,23 +3,15 @@ def es_palindromo(palabra):
 	pila = []
 	cola = deque([])
 	palindromo = (" ")
-	verdadero = True
 	for letra in palabra:
 		pila.append(letra) # Pop
 		cola.append(letra) # popleft
-	maximo = len(pila)
-	while verdadero:
-		if maximo >= 0:
-			palindromo = palindromo + pila[maximo]
-			maximo = maximo - 1
-		else:
-			verdadero = False
-	if palindromo == palabra:
-		return True
-	else:
-		return False
+	for i in range(len(palabra)):
+		if cola.popleft() != pila.pop():
+			return False
+	return True
 
 
 
 nombre = ("ana")
-print (es_palindromo(palabra))
+print (es_palindromo(nombre))
