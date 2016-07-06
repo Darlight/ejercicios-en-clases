0cbddc3556748f4cad14aa797ec44af2c6b5a74a
Darlight <andres.perdomo09@gmail.com>
2016-07-06 14:23:08 -0600

    Totito en proceso

---
+++ Totito/totito.py
@@ -0,0 +1,12 @@
+lista = [ ["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"] ]
+print ("Bienvenido a Totito")
+
+jugador_1 = input("Jugador 1 Ingrese su nombre: ")
+jugador_2 = input("Jugador 2 Ingrese su nombre: ")
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+
+print(jugador_1)
+ubicacion_x = input ("Que lugar quieres poner X")
