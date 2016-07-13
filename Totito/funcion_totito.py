f569f329375ccbccd1026684a6f55c166991c2d1
Darlight <andres.perdomo09@gmail.com>
2016-07-13 08:40:55 -0600

    En busca de encontrar un codigo binario

---
+++ Totito/funcion_totito.py
@@ -0,0 +1,3 @@
+def ganador(turnos):
+	suma = []
+	
--- Totito/totito.py
+++ Totito/totito.py
@@ -1,41 +1,40 @@
-from funcion_totito.py import conseguir_ganador
 from random import random
-lista = [[ "A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"] ]]
+lista = [[ "A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"] ]
+turnos = [[0,0,0],[0,0,0],[0,0,0]]
+posicion_tomada = []
 
 print ("Bienvenidos a Totito")
-
-jugador_1 = input("Jugador 1 Ingrese su nombre: ")
-jugador_2 = input("Jugador 2 Ingrese su nombre: ")
-
-print(lista[0])
-print(lista[1])
-print(lista[2])
-
-tiro = input("En que posicion quiere ponerlo: ")
-tiro_valor = ord(tiro)-65
-tiro_fila = tiro_valor % 3
-tiro_columna = tiro_valor //3
-
-while conseguir_ganador:
-	tiro = input("En que posicion quiere poner X: ")
+b = 4
+while b != 5:
+	print(lista[0])
+	print(lista[1])
+	print(lista[2])
+		
+	tiro = input(" Jugador 1, En que posicion quiere poner X: ")
 	tiro_valor = ord(tiro)-65
 	tiro_fila = tiro_valor % 3
 	tiro_columna = tiro_valor //3
 
-	lista[tiro_columna][tiro_fila] = chr(88)
+	if tiro_valor in posicion_tomada:
+			print("Use otra posicion")
+	else:
+		posicion_tomada.append(tiro_valor)
+		lista[tiro_columna][tiro_fila] = chr(88)
+
 	print(lista[0])
 	print(lista[1])
 	print(lista[2])
 
-	tiro = input("En que posicion quiere poner X: ")
+	tiro = input("Jugador 2, En que posicion quiere poner O: ")
 	tiro_valor = ord(tiro)-65
 	tiro_fila = tiro_valor % 3
 	tiro_columna = tiro_valor //3
 
-	lista[tiro_columna][tiro_fila] = chr()
-	print(lista[0])
-	print(lista[1])
-	print(lista[2])
+	if tiro_valor in posicion_tomada:
+			print("Use otra posicion")
+	else:
+		posicion_tomada.append(tiro_valor)
+		lista[tiro_columna][tiro_fila] = chr(79)
 
 
 
