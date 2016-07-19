4870419f5bbdf9b24ab3e29dc0ac81c85e9af622
Darlight <andres.perdomo09@gmail.com>
2016-07-19 08:48:13 -0600

    Ultimas pruebas en el totito

--- Totito/funcion_totito.py
+++ Totito/funcion_totito.py
@@ -3,12 +3,12 @@ def ganador(totito):
 	for fila in totito:
 		suma.append(sum(fila))
 	for columna in range(3):
-		suma.append(totito[0][columna]+totito[1][columna]+[2][columna])
+		suma.append(totito[0][columna] + totito[1][columna] + totito[2][columna])
 	sumad1 = 0
 	sumad2 = 0
 	for diagonal in range(3):
-		sumad1 += totito[diagonal][diagonal]
-		sumad2 += totito[diagonal][2-diagonal]
+		sumad1 += (totito[diagonal][diagonal])
+		sumad2 += (totito[diagonal][2-diagonal])
 		suma.append(sumad1)
 		suma.append(sumad2)
 	if 3 in suma:
@@ -17,16 +17,3 @@ def ganador(totito):
 		return -1
 	else:
 		return False 
-
-
-def print_totito(totito):
-	resultado = " "
-	for i in range(3):
-		for j in range(3):
-			celda = totito(i)(j)
-			if celda == 1:
-				return "X"
-			elif celda == -1:
-				return "O"
-			else:
-				chr((3*i)+j+65)
--- Totito/totito.py
+++ Totito/totito.py
@@ -1,25 +1,39 @@
-from funcion_totito import ganador, print_totito
-lista = [[ ord("A"), ord("B"), ord("C")], [ord("D"), ord("E"), ord("F")], [ord("G"), ord("H"), ord("I")] ]
+from funcion_totito import ganador
+lista = [[ 65, 66, 67], [68, 69, 70], [71, 72, 73]]
+posicion_tomada = []
 
 print ("Bienvenidos a Totito\n")
-while ganador(lista):
 
-	print(lista[0])
-	print(lista[1])
-	print(lista[2])
+while ganador(lista) == 1 or ganador(lista) == -1 or ganador(lista) == False:
+
+	print (chr(lista[0][0]), chr(lista[0][1]), chr(lista[0][2]))
+	print (chr(lista[1][0]), chr(lista[1][1]), chr(lista[1][2]))
+	print (chr(lista[2][0]), chr(lista[2][1]), chr(lista[2][2]))
+
 		
 	tiro = input(" Jugador 1, En que posicion quiere poner X: ")
 	tiro_valor = ord(tiro)-65
 	tiro_fila = tiro_valor % 3
 	tiro_columna = tiro_valor //3
 
-	print(print_totito(lista))
+	if tiro_valor in posicion_tomada:
+			print("Use otra posicion perdio el turno \n")
+	else:
+		posicion_tomada.append(tiro_valor)
+		lista[tiro_columna][tiro_fila] = 88
 
-	print(lista[0])
-	print(lista[1])
-	print(lista[2])
+
+	print (chr(lista[0][0]), chr(lista[0][1]), chr(lista[0][2]))
+	print (chr(lista[1][0]), chr(lista[1][1]), chr(lista[1][2]))
+	print (chr(lista[2][0]), chr(lista[2][1]), chr(lista[2][2]))
 
 	tiro = input("Jugador 2, En que posicion quiere poner O: ")
 	tiro_valor = ord(tiro)-65
 	tiro_fila = tiro_valor % 3
 	tiro_columna = tiro_valor //3
+
+	if tiro_valor in posicion_tomada:
+		print("Use otra posicion perdio el turno \n")
+	else:
+		posicion_tomada.append(tiro_valor)
+		lista[tiro_columna][tiro_fila] = 79
