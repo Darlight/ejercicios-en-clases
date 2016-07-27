213ac63e422f38e912feb6b1165c653892b91223
Darlight <andres.perdomo09@gmail.com>
2016-07-27 08:53:07 -0600

    muñeco

--- Ahorcado/ahorcado.py
+++ Ahorcado/ahorcado.py
@@ -7,25 +7,28 @@ palabra_elegida = random.choice(lista_de_palabras)
 oportunidades = 0
 ganador = False
 letras_separadas = list(palabra_elegida)
-error = False
 # usar "letra" in "palabra" para que el juego funcione.
 # crear el stickman con: "O", "I", "/", "\" y el palo.
 for guion in range(len(letras_separadas)):
 	letras_separadas[guion] = "_"
 while oportunidades != 7 or ganador == True:
 	estado = False
-	print(letras_separadas)
+	print(" ".join(letras_separadas))
 
 	letra_ingresada = input("Ingrese la letra deseada: ")
+	encontrado = False
 	if letra_ingresada in palabra_elegida:
 		for i in range(len(palabra_elegida)):
 			if palabra_elegida[i] == letra_ingresada:
 				letras_separadas[i] = letra_ingresada
+				encontrado = True
 	else:
-		oportunidades = oportunidades + 1
-		error = True
-		dibujo(error)
-
+		estado = True
+		oportunidades += 1
+		print(dibujo(estado,oportunidades))
+		resultados = 6 - oportunidades
+		print("Numero de oportunidades = " + str(resultados))
+print("perdio...")
 
 
 
