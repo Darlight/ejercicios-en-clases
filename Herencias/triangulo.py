974fd94fa41caa9b5c24e0983bcaf1d4159cbf5d
Darlight <andres.perdomo09@gmail.com>
2016-08-09 08:46:39 -0600

    Herencias

---
+++ Herencias/triangulo.py
@@ -0,0 +1,16 @@
+from jesus import FiguraGeometrica
+
+class Triangulo(FiguraGeometrica):
+
+	def __init__(self, base, altura):
+		super().__init__(base,altura)
+
+	def imprimir(self):
+		resultado = " "
+
+		for i in range(self.altura):
+			resultado += "*" + (i + 1) * "\n"
+			return resultado
+
+	def calcular_area(self):
+		return super().calcular.area()/ 2.0
