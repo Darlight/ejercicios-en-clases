7ca3a0b73974c31d39865ff62ae831638d244fed
Darlight <andres.perdomo09@gmail.com>
2016-08-11 08:42:44 -0600

    ejercicio terminado

--- Herencias/uso_figuras.py
+++ Herencias/uso_figuras.py
@@ -1,21 +1,30 @@
 from cuadrado import Cuadrado
-from jesus import FiguraGeometrica
 from triangulo import Triangulo
 
 desea_continuar = False
 while desea_continuar == False:
-	respuesta = int(input("1. Crear Figura \n 2. Salir"))
+	respuesta = int(input("1. Crear Figura \n 2. Salir "))
 	if respuesta == 1:
-		figura = int(input("1. Cuadrado \n 2. Triangulo"))
+		figura = int(input("1. Cuadrado \n 2. Triangulo "))
 		if figura == 1:
-			lados = int(input("Valor de los lados: "))
-			respuesta_2 = int(input("1. Calcular el area \n 2. Imprimir figura"))
+			lado = int(input("Valor de los lados: "))
+			cuadrado = Cuadrado(lado)
+			respuesta_2 = int(input("1. Calcular el area \n 2. Imprimir figura "))
 			if respuesta_2 == 1:
-				print ("Area: ",lados.calcular_area())
+				print ("Area: ", cuadrado.calcular_area())
 			elif respuesta_2 == 2:
-				print ("Figura: " ,lados.imprimit())
-
-
+				print ("Figura:" )
+				print(cuadrado.imprimir())
+		if figura == 2:
+			base = int(input("Valor de base: "))
+			altura = int(input("Valor de altura: "))
+			triangulo = Triangulo(base,altura)
+			respuesta_2 = int(input("1. Calcular el area \n 2. Imprimir figura "))
+			if respuesta_2 == 1:
+				print ("Area: ", triangulo.calcular_area())
+			elif respuesta_2 == 2:
+				print ("Figura:" )
+				print(triangulo.imprimir())
 	else: 
 		desea_continuar = True
 print("Gracias por el servicio usado")
